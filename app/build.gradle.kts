389316fbd6e4d5588caaf8ba9eb1405ad85b4dcf
Hamjoshua <biryukov-10@list.ru>
2024-11-29 15:05:22 +0700

    Работающий запрос

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("androidx.core:core-ktx:1.9.0")
--- app/src/main/java/com/example/retrofitforecaster/MainActivity.kt
+++ app/src/main/java/com/example/retrofitforecaster/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.retrofitforecaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch{
+            val daysApi = RetrofitHelper.getInstance().create(DayGetter::class.java)
+
+            val days = daysApi.check()
+            if(days != null){
+
+            }
+        }
+
     }
 }
+
+interface DayGetter {
+    @GET("forecast?q=Shklov,by&appid=${BuildConfig.API_KEY_OPEN_WEATHER_MAP}")
+    suspend fun check() : Response<DataResponce>
+}
+
+object RetrofitHelper {
+    val baseUrl = "http://api.openweathermap.org/data/2.5/"
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
+
+data class Main(
+    @SerializedName("temp") val temp: Double
+)
+data class Weather(
+    @SerializedName("main") val main: String,
+    @SerializedName("icon") val icon: String
+)
+data class DayPrognosis (
+    @SerializedName("dt_text") val dt_text: String,
+    @SerializedName("main") val main: Main,
+    @SerializedName("weather") val weather: ArrayList<Weather>
+)
+
+data class DataResponce(
+    @SerializedName("list") val list: ArrayList<DayPrognosis>
+)
